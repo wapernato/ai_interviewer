7fe3152b8ac500e4d16d2a61f3fbccf343f29422
wapernato <rodion080797@gmail.com>
2026-07-26 17:47:42 +0300

    added tests for new logic UserController

--- api-tests-kotlin/src/test/kotlin/tests/user/UserGetApiTest.kt
+++ api-tests-kotlin/src/test/kotlin/tests/user/UserGetApiTest.kt
@@ -1,6 +1,7 @@
 package tests.user
 
 import config.ApiConfig
+import dto.auth.LoginRequest
 import dto.auth.RegisterRequest
 import io.restassured.RestAssured.given
 import io.restassured.http.ContentType.JSON
@@ -12,7 +13,7 @@ import kotlin.test.Test
 class UserGetApiTest {
 
     @Test
-    fun `get user by id should return user when user exists`(){
+    fun `get me should return current user when token is valid`(){
         val uniqueSuffix = System.currentTimeMillis()
 
         val username = "ximeo_$uniqueSuffix"
@@ -39,11 +40,31 @@ class UserGetApiTest {
 
         val userId = response.jsonPath().getLong("id")
 
+        val loginBody = LoginRequest(
+            email = email,
+            password = password
+        )
+
+        val loginResponse = given()
+            .baseUri(ApiConfig.baseUrl)
+            .contentType(JSON)
+            .accept(JSON)
+            .body(loginBody)
+        .`when`()
+            .post("/api/auth/login")
+        .then()
+            .statusCode(200)
+            .extract()
+            .response()
+
+        val token = loginResponse.jsonPath().getString("token")
+
         given()
             .baseUri(ApiConfig.baseUrl)
             .accept(JSON)
+            .auth().oauth2(token)
         .`when`()
-            .get("/api/users/$userId")
+            .get("/api/me")
         .then()
             .statusCode(200)
             .body("username", equalTo(username))
@@ -52,12 +73,5 @@ class UserGetApiTest {
             .body("role", equalTo("USER"))
             .body("$", not(hasKey("password")))
             .body("$", not(hasKey("passwordHash")))
-
-        given()
-            .baseUri(ApiConfig.baseUrl)
-        .`when`()
-            .delete("/api/users/$userId")
-        .then()
-            .statusCode(204)
     }
 }
